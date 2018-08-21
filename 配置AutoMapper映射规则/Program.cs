b0990fc6eeabdbbf87d742788ccf93a09c22d41b
云行 邓 <271293794@qq.com>
2018-08-21 13:55:37 +0800

    添加项目文件。

---
+++ 配置AutoMapper映射规则/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 配置AutoMapper映射规则
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
